185cdb5d9c42f7dedaa838daae55c1e90f6fa550
krishnashuk <shuklakrishna.kris@gmail.com>
2024-12-28 12:44:11 +0530

    Correct version

--- JwtAuth/index.js
+++ JwtAuth/index.js
@@ -4,6 +4,9 @@ const jwt = require("jsonwebtoken");
 const JWT_SECRET = "USER_APP";
 
 const app = express();
+app.use(express.json());
+
+const users = [];
 
 app.post("/signin", function(req,res){
   const username = req.body.username;
@@ -12,44 +15,65 @@ app.post("/signin", function(req,res){
   let foundUser = null;
 
   for(let i = 0; i < users.length; i++){
-    if(users[i].username === username && users[i].password ===password){
+    if(users[i].username === username && users[i].password === password){
         foundUser = user[i]
     }
   }
 
   if(!foundUser){
     res.json({
-        message: "Credentilas incorrect"
+        message: "Credentials incorrect"
     })
     return
    
   } else {
     const token = jwt.sign({
-        username
+        username: "raman"
     }, JWT_SECRET);
 
     res.json({
         token: token
     })
     }
-  }
+  })
 
  
-};
+
 
 app.post("/signup", function(req,res){
     const username = req.body.username
     const password = req.body.username
     users.push({
         username: username,
-        password:password
+        password: password
     })
+     
+    
 
     res.json({
         message: "you are signed in"
     })
 })
 
-app.post("/me", function(req,res){
-    
+app.get("/me", function(req,res){
+    const token = req.headers.token;
+
+    const decodedData = jwt.verify(token, JWT_SECRET);
+
+    if (decodedData.username){
+      let foundUser = null;
+
+      for(let i = 0; i< users.length; i++){
+        if(users[i].username === decodedData.username){
+          foundUser = user[i]
+        }
+      }
+
+      res.json({
+        username: foundUser.username,
+        password: foundUser.password
+      })
+    }
 })
+
+app.listen(3000);
